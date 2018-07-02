36d803bb619ac6452c64b30e6fcf6d07f6fa959c
ianagez <ianagez88@gmail.com>
2018-07-02 10:44:51 +0300

    cipherLab_inventory_final

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,14 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
-    "id": "cordova-plugin-keyboard.keyboard",
-    "file": "plugins/cordova-plugin-keyboard/www/keyboard.js",
-    "pluginId": "cordova-plugin-keyboard",
-    "clobbers": [
-      "window.Keyboard"
-    ]
-  },
-  {
     "id": "cordova-plugin-cipherlab-rs30.CipherlabRS30CordovaPlugin",
     "file": "plugins/cordova-plugin-cipherlab-rs30/www/CipherlabRS30CordovaPlugin.js",
     "pluginId": "cordova-plugin-cipherlab-rs30",
@@ -20,7 +12,6 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-keyboard": "1.2.0",
   "cordova-plugin-whitelist": "1.3.3",
   "cordova-plugin-cipherlab-rs30": "0.0.3"
 };
