d82249000fac8a12f41afe49d87cbb12d7b562f0
ianagez <ianagez88@gmail.com>
2018-07-02 10:18:56 +0300

    zebra plugin bug in process

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,20 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "com.jkt.zebra.barcode.plugin.ZebraBarcodePlugin",
+    "file": "plugins/com.jkt.zebra.barcode.plugin/www/ZebraBarcodePlugin.js",
+    "pluginId": "com.jkt.zebra.barcode.plugin",
+    "clobbers": [
+      "zebraBarcodePlugin"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "com.jkt.zebra.barcode.plugin": "0.0.1",
+  "cordova-android-support-gradle-release": "1.4.4",
+  "cordova-plugin-whitelist": "1.3.3"
+};
+// BOTTOM OF METADATA
+});
